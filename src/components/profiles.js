9aa7abec9b674cbf45475a596c62f216404af7dc
Omar Raéf <omarraef1@email.arizona.edu>
2023-06-09 22:50:58 +0200

    added progress bars

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -14,9 +14,24 @@ function Item(data){
 
         <>
             {
+
                 
                 data.map((value, index) => (
+                    
                     <div className="flex" key={index}>
+
+                        {/* <div className="skill-box">
+                            <span>HTML</span>
+                            <div className="skill-bar">
+                                <span className="skill-per html">
+                                    <span className="tooltip">95%</span>
+
+                                </span>
+                            </div>
+
+                        </div> */}
+
+
                         <div className="item">
                             <img src={value.img} alt="img__" />
             
@@ -24,12 +39,22 @@ function Item(data){
                                 <h3 className='name text-dark'>{value.name}</h3>    
                                 <span>{value.location}</span>
                             </div>                
-                        </div>
+                            </div>
 
-                        <div className="info">
-                            <p><b>Weekly: </b>{value.week_score}</p>
+                            <div className="skill-box">
+                            <div className="skill-bar">
+                            <span className="skill-per html">
+                                <span className="tooltip"><p><b>Weekly: </b>{value.week_score/2}</p></span>
+                            </span>
+                            </div>
+                            </div>
+                        
+
+                        {/* <div className="info">
                             <p><b>All Time: {value.score}</b></p>
-                        </div>
+                        </div> */}
+
+
                     </div>
                     )
                 )
