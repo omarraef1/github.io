9a5c94a0173cb0a355c6d63248d81a48b533fc61
Omar Raéf <omarraef1@email.arizona.edu>
2023-06-15 23:57:59 +0200

    added neww scores

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -5,80 +5,73 @@ import { Col, ProgressBar, Row } from 'react-bootstrap'
 const DataDisplay = () => {
     const Leaderboard = [
         {
-            name: "Shawn Hanna",
-            location: "Amsterdam",
-            score: 1550,
-            week_score: 21,
-            img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-            dt: "2023-06-05"
-        },
-        {
-            name: "Fidel Hand",
-            location: "New York",
-            score: 2310,
-            week_score: 22,
-            img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-            dt: "2023-06-05"
-        },
-        {
-            name: "Bessie Hickle",
-            location: "Beijing",
-            score: 350,
-            week_score: 23,
-            img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-            dt: "2023-06-05"
-        },
-        {
-            name: "Adella Wunsch",
-            location: "Tokyo",
-            score: 2100,
-            week_score: 24,
-            img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-            dt: "2023-06-05"
-        },
-        {
-            name: "Clair O'Connell",
-            location: "London",
-            score: 1250,
-            week_score: 25,
-            img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-            dt: "2023-06-05"
-        },
-
-        {
-            name: "Kameron Prosacco",
-            location: "Toronto",
-            score: 5250,
-            week_score: 26,
-            img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-            dt: "2023-06-05"
+            name: "Daniel Supmitchotima",
+            score: 0,
+            week_score: 0,
+            img: ""
         }
         ,
         {
-            name: "Omar Raef",
-            location: "Cairo",
-            score: 2100,
-            week_score: 219,
-            img: "https://drive.google.com/uc?export=view&id=1wBbR9TRtmpSxmYsdJ7qUuIDCZQMLxrA5",
-            dt: "2023-06-05"
+            name: "Destiny Wu",
+            score: 0,
+            week_score: 0,
+            img: ""
         }
         ,
         {
-            name: "Sarah Cortes",
-            location: "San Francisco",
-            score: 10000,
-            week_score: 200,
-            img: "https://drive.google.com/uc?export=view&id=1DcUo-xnB_bdWhhBTk3URC-IvdqHD_KtY",
-            dt: "2023-06-05"
+            name: "Aaron Lui",
+            score: 0,
+            week_score: 0,
+            img: ""
         }
         ,
         {
-            name: "Omar Adel",
-            location: "Zahraa Maadi",
-            score: 70000,
-            week_score: 600,
-            img: "https://drive.google.com/uc?export=view&id=1g9pOA7s-ETKrKUGuzi9ghXv9K2P-XG1N",
-            dt: "2023-06-05"
+            name: "Jonathan Vi",
+            score: 0,
+            week_score: 0,
+            img: ""
+        }
+        ,
+        {
+            name: "Vi Au",
+            score: 0,
+            week_score: 0,
+            img: ""
+        }
+        ,
+        {
+            name: "Lillian Chan",
+            score: 0,
+            week_score: 0,
+            img: ""
+        }
+        ,
+        {
+            name: "Anastasiia Kotsiuruba",
+            score: 0,
+            week_score: 0,
+            img: ""
+        }
+        ,
+        {
+            name: "Clara Ng",
+            score: 0,
+            week_score: 0,
+            img: ""
+        }
+        ,
+        {
+            name: "Julianna Chiu",
+            score: 0,
+            week_score: 0,
+            img: ""
+        }
+        ,
+        {
+            name: "Rebecca Mei",
+            score: 0,
+            week_score: 0,
+            img: ""
         }
     ];
 
