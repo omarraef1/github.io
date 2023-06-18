8cc335ada1d76d25d0824bc643436fd27f1cf38e
Omar Raéf <omarraef1@email.arizona.edu>
2023-06-18 07:11:51 +0200

    wk1 scores

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -6,16 +6,16 @@ const DataDisplay = () => {
     const Leaderboard = [
         {
             name: "Daniel Supmitchotima",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 1100,
+            week_score: 1100,
+            img: "https://drive.google.com/uc?export=view&id=1qQ4WR9C00UBKRhDJ1dztLdJbSDlstzTe"
         }
         ,
         {
             name: "Destiny Wu",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 2350,
+            week_score: 2350,
+            img: "https://drive.google.com/uc?export=view&id=1lqHlWChsNSK-okF7xpjdcgtl4RFNLIYZ"
         }
         ,
         {
@@ -27,51 +27,51 @@ const DataDisplay = () => {
         ,
         {
             name: "Jonathan Vi",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 1350,
+            week_score: 1350,
+            img: "https://drive.google.com/uc?export=view&id=13WzRvhOY3iMW-vO5KhKzBwNpvAv-9yUN"
         }
         ,
         {
-            name: "Vi Au",
-            score: 0,
-            week_score: 0,
-            img: ""
+            name: "Vincent Au",
+            score: 1850,
+            week_score: 1850,
+            img: "https://drive.google.com/uc?export=view&id=1GZgWg9Iz-AdvTULE1fcXPoIp6ZVLYRm_"
         }
         ,
         {
             name: "Lillian Chan",
-            score: 0,
-            week_score: 0,
+            score: 700,
+            week_score: 700,
             img: ""
         }
         ,
         {
             name: "Anastasiia Kotsiuruba",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 850,
+            week_score: 850,
+            img: "https://drive.google.com/uc?export=view&id=1S7ZI-Q2IjMBbLvBIh07E2LJvQkX08NQ6"
         }
         ,
         {
             name: "Clara Ng",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 2350,
+            week_score: 2350,
+            img: "https://drive.google.com/uc?export=view&id=1Z2snP9zWzM-p1lftV_VCc5Tp-kdhfgnG"
         }
         ,
         {
             name: "Julianna Chiu",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 500,
+            week_score: 500,
+            img: "https://drive.google.com/uc?export=view&id=17g4yuK0xZJPQYdnE1aWSJxC7WgMlzmjJ"
         }
         ,
         {
             name: "Rebecca Mei",
-            score: 0,
-            week_score: 0,
-            img: ""
+            score: 1200,
+            week_score: 1200,
+            img: "https://drive.google.com/uc?export=view&id=1J9iapVO2YWDHkNfHegkY57wybGLWRvMp"
         }
     ];
 
@@ -95,7 +95,7 @@ const DataDisplay = () => {
             <div id="profile">
 
                 <div className="duration">
-                    <button onClick={() => handleButtonClick('score')}>All Times</button>
+                    <button onClick={() => handleButtonClick('score')}>Cumulative</button>
                     <button onClick={() => handleButtonClick('week_score')}>Weekly</button>
                 </div>
 
@@ -118,8 +118,8 @@ const DataDisplay = () => {
                                 <>
                                     <div className="info">
                                         <Row>
-                                            <Col sm={2}><p><small>All Times:</small></p></Col>
-                                            <Col sm={10}><ProgressBar min={10} max={10000} now={item.score} label={`${item.score} PTS`} /></Col>
+                                            <Col sm={2}><p><small>Cumulative:</small></p></Col>
+                                            <Col sm={10}><ProgressBar min={10} max={2350} now={item.score} label={`${item.score} PTS`} /></Col>
                                         </Row>
                                     </div>
                                 </>
@@ -128,7 +128,7 @@ const DataDisplay = () => {
                                     <div className="info">
                                         <Row>
                                             <Col sm={2}><p><small>Weekly:</small></p></Col>
-                                            <Col sm={10}><ProgressBar min={1} max={100} now={item.week_score} label={`${item.week_score} PTS`} /></Col>
+                                            <Col sm={10}><ProgressBar min={1} max={2350} now={item.week_score} label={`${item.week_score} PTS`} /></Col>
                                         </Row>
                                     </div>
                                 </>
