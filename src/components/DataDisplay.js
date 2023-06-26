66669601e2fa8b959593c6b57214ef1e312e2b51
Omar Raéf <omarraef1@email.arizona.edu>
2023-06-26 15:48:09 +0200

    updated intern scores

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -6,71 +6,71 @@ const DataDisplay = () => {
     const Leaderboard = [
         {
             name: "Daniel Supmitchotima",
-            score: 3350,
-            week_score: 3350,
+            score: 5350,
+            week_score: 2000,
             img: "https://drive.google.com/uc?export=view&id=1qQ4WR9C00UBKRhDJ1dztLdJbSDlstzTe"
         }
         ,
         {
             name: "Destiny Wu",
-            score: 3850,
-            week_score: 3850,
+            score: 6850,
+            week_score: 3000,
             img: "https://drive.google.com/uc?export=view&id=1lqHlWChsNSK-okF7xpjdcgtl4RFNLIYZ"
         }
         ,
         {
             name: "Aaron Lui",
-            score: 0,
-            week_score: 0,
+            score: 5050,
+            week_score: 5050,
             img: "https://drive.google.com/uc?export=view&id=1M4zjzWkramzOU19_ReHfN5Kg4YmcLLEF"
         }
         ,
         {
             name: "Jonathan Vi",
-            score: 1950,
-            week_score: 1950,
+            score: 4450,
+            week_score: 2500,
             img: "https://drive.google.com/uc?export=view&id=13WzRvhOY3iMW-vO5KhKzBwNpvAv-9yUN"
         }
         ,
         {
             name: "Vincent Au",
-            score: 1850,
-            week_score: 1850,
+            score: 4350,
+            week_score: 2500,
             img: "https://drive.google.com/uc?export=view&id=1GZgWg9Iz-AdvTULE1fcXPoIp6ZVLYRm_"
         }
         ,
         {
             name: "Lillian Chan",
-            score: 2850,
-            week_score: 2850,
+            score: 5850,
+            week_score: 3000,
             img: "https://drive.google.com/uc?export=view&id=1rzz53iQQdmRKYfpeRMDBPme8I_guAueS"
         }
         ,
         {
             name: "Anastasiia Kotsiuruba",
-            score: 2350,
-            week_score: 2350,
+            score: 2850,
+            week_score: 500,
             img: "https://drive.google.com/uc?export=view&id=1S7ZI-Q2IjMBbLvBIh07E2LJvQkX08NQ6"
         }
         ,
         {
             name: "Clara Ng",
-            score: 4350,
-            week_score: 4350,
+            score: 7850,
+            week_score: 3500,
             img: "https://drive.google.com/uc?export=view&id=1Z2snP9zWzM-p1lftV_VCc5Tp-kdhfgnG"
         }
         ,
         {
             name: "Julianna Chiu",
-            score: 500,
-            week_score: 500,
+            score: 1550,
+            week_score: 1050,
             img: "https://drive.google.com/uc?export=view&id=17g4yuK0xZJPQYdnE1aWSJxC7WgMlzmjJ"
         }
         ,
         {
             name: "Rebecca Mei",
-            score: 1550,
-            week_score: 1550,
+            score: 2550,
+            week_score: 1000,
             img: "https://drive.google.com/uc?export=view&id=1J9iapVO2YWDHkNfHegkY57wybGLWRvMp"
         }
     ];
@@ -119,7 +119,7 @@ const DataDisplay = () => {
                                     <div className="info">
                                         <Row>
                                             <Col sm={2}><p><small>Cumulative:</small></p></Col>
-                                            <Col sm={10}><ProgressBar min={10} max={4350} now={item.score} label={`${item.score} PTS`} /></Col>
+                                            <Col sm={10}><ProgressBar min={10} max={7850} now={item.score} label={`${item.score} PTS`} /></Col>
                                         </Row>
                                     </div>
                                 </>
@@ -128,7 +128,7 @@ const DataDisplay = () => {
                                     <div className="info">
                                         <Row>
                                             <Col sm={2}><p><small>Weekly:</small></p></Col>
-                                            <Col sm={10}><ProgressBar min={1} max={4350} now={item.week_score} label={`${item.week_score} PTS`} /></Col>
+                                            <Col sm={10}><ProgressBar min={1} max={5050} now={item.week_score} label={`${item.week_score} PTS`} /></Col>
                                         </Row>
                                     </div>
                                 </>
